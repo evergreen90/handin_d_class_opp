0d5202b581525cfedce05f9fea1d4a1edd7e422f
evergreen90 <evergreen90@gmail.com>
2025-08-15 12:36:17 +0900

    d-1.2を回答

---
+++ d-1.py
@@ -0,0 +1,28 @@
+# 次のコードが正しく動作するような Circle クラスを実装してください
+# area は面積、 perimeter は周囲長(=円周の長さ) という意味です
+import math
+
+
+class Circle:
+    #コストラクタ
+    def __init__(self,radius):
+        self.radius = radius
+    
+    #areaメソッド
+    def area(self):
+        return f"{self.radius ** 2 * math.pi:.2f}"
+    
+    #perimeterメソッド
+    def perimeter(self):
+        return f"{self.radius * 2 * math.pi:.2f}"
+
+
+# 半径1の円
+circle1 = Circle(radius=1)
+print(circle1.area())  # 3.14
+print(circle1.perimeter())  # 6.28
+
+# 半径3の円
+circle3 = Circle(radius=3)
+print(circle3.area())  # 28.27
+print(circle3.perimeter())  # 18.85
