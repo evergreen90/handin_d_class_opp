f6345ad76d63a7ac4eb3be5663a49b8fe08ef1fe
evergreen90 <evergreen90@gmail.com>
2025-08-15 15:32:33 +0900

    d-6まで回答

---
+++ d-4.py
@@ -0,0 +1,28 @@
+# 次のコードが正しく動作するような MyCounterV1 クラスを実装してください
+
+class MyCounterV1:
+    #コストラクタ
+    def __init__(self,value):
+        self.value = value
+    
+    #countupメソッド
+    def count_up(self):
+        self.value += 1
+
+counter1 = MyCounterV1(value=0)
+print(counter1.value)  # 0
+
+counter1.count_up()
+print(counter1.value)  # 1
+
+counter1.count_up()
+print(counter1.value)  # 2
+
+counter2 = MyCounterV1(value=7)
+print(counter2.value)  # 7
+
+counter2.count_up()
+print(counter2.value)  # 8
+
+counter2.count_up()
+print(counter2.value)  # 9
